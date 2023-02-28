bccd41b1c1f49f9b44ce27cf46cbe7dd594f4dac
Mike <mike.simnes@gmail.com>
2023-02-27 18:20:00 -0800

    created fetchISSFlyOverTimes, functional and commented out

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 // const { fetchMyIP } = require('./iss');
 // const { fetchCoordsByIp } = require('./iss');
+const { fetchISSFlyOverTimes } = require('./iss');
+
 // currently commented out but working
 // fetchMyIP((error, ip) => {
 //   if (error) {
@@ -12,10 +14,19 @@
 
 
 // currently commented out but working
-// fetchCoordsByIp('34.82.93.10', (error, coords) => {
+// fetchCoordsByIp(ip, (error, coords) => {
 //   if (error) {
 //     console.log("It didn't work: ", error);
 //     return;
 //   }
 //   console.log("It worked! Returned location coordinates :", coords);
+// });
+
+// currently commented out but working
+// fetchISSFlyOverTimes(coords, (error, times) => {
+//   if (error) {
+//     console.log("It didn't work: ", error);
+//     return;
+//   }
+//   console.log("It worked! Returned flyover times :", times);
 // });
