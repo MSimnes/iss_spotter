5aaf2fd9074c0a2be63b970af029024520cbe4a0
Mike <mike.simnes@gmail.com>
2023-02-27 20:02:14 -0800

    added then blocks to nextISS function and exported it

--- iss_promised.js
+++ iss_promised.js
@@ -9,8 +9,20 @@ const fetchCoordsByIP = function(body) {
   return request(`http://ipwho.is/${parsedIP}`);
 };
 
-
-module.exports = {
-  fetchMyIp,
-  fetchCoordsByIP
+const fetchISSFlyOverTimes = function(body) {
+  const {latitude, longitude} = JSON.parse(body);
+  const URL = `https://iss-flyover.herokuapp.com/json/?lat=${latitude}&lon=${longitude}`;
+  return request(URL);
 };
+
+const nextISSTimesForMyLocation = function() {
+  return fetchMyIp()
+    .then(fetchCoordsByIP)
+    .then(fetchISSFlyOverTimes)
+    .then((data) => {
+      const { response } = JSON.parse(data);
+      return response;
+    });
+};
+
+module.exports = { nextISSTimesForMyLocation };
