21eed30c9bc4a42796bb7a0e1874c8b7a15e48ce
Mike <mike.simnes@gmail.com>
2023-02-27 18:03:10 -0800

    added callback, working but commented out for tests

--- index.js
+++ index.js
@@ -1,10 +1,21 @@
-const { fetchMyIP } = require('./iss');
+// const { fetchMyIP } = require('./iss');
+// const { fetchCoordsByIp } = require('./iss');
+// currently commented out but working
+// fetchMyIP((error, ip) => {
+//   if (error) {
+//     console.log("It didn't work!" , error);
+//     return;
+//   }
 
-fetchMyIP((error, ip) => {
-  if (error) {
-    console.log("It didn't work!" , error);
-    return;
-  }
+//   console.log('It worked! Returned IP:' , ip);
+// });
 
-  console.log('It worked! Returned IP:' , ip);
-});
+
+// currently commented out but working
+// fetchCoordsByIp('34.82.93.10', (error, coords) => {
+//   if (error) {
+//     console.log("It didn't work: ", error);
+//     return;
+//   }
+//   console.log("It worked! Returned location coordinates :", coords);
+// });
